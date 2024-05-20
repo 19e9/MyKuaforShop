843391e6cdb068117803dc735feb09cfd1606545
~ Khal!d <Khaledtg17@gmail.com>
2024-05-20 14:11:28 +0300

    Implement user registration with Firebase Authentication.

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mykuaforshop/pages/home.dart';
 import 'package:mykuaforshop/pages/login.dart';
 
 class SingUp extends StatefulWidget {
@@ -10,11 +12,43 @@ class SingUp extends StatefulWidget {
 }
 
 class _SingUpState extends State<SingUp> {
+  String? name, mail, password;
+
   TextEditingController namecontroller = new TextEditingController();
   TextEditingController emailcontroller = new TextEditingController();
   TextEditingController passwordcontroller = new TextEditingController();
 
   final _formkey = GlobalKey<FormState>();
+
+  registration() async {
+    if (password != null && name != null && mail != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: mail!, password: password!);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "Üyeliğiniz başarıyla oluşturuldu",
+          style: TextStyle(fontSize: 20.0),
+        )));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Home()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'Girdiniz şifre çok zayif') {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+            "Girdiğiniz Şifre çok zayıf",
+            style: TextStyle(fontSize: 20.0),
+          )));
+        } else if (e.code == "E-posta zaten kullanımda") {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+            "Girdiğiniz e-posta adresiyle zaten bir hesap mevcutur",
+            style: TextStyle(fontSize: 20.0),
+          )));
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,133 +85,136 @@ class _SingUpState extends State<SingUp> {
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(40.0),
                       topRight: Radius.circular(40.0))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Ad",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Lütfen Ad ve Soyad giriniz';
-                      }
-                      return null;
-                    },
-                    controller: namecontroller,
-                    decoration: InputDecoration(
-                        hintText: "Adınız",
-                        prefixIcon: Icon(Icons.person_outline)),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    "E-posta",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Lütfen E-posta giriniz';
-                      }
-                      return null;
-                    },
-                    controller: emailcontroller,
-                    decoration: InputDecoration(
-                        hintText: "E-posta adresinizi girin",
-                        prefixIcon: Icon(Icons.mail_outline)),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    "Şifre",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Lütfen güçlü bir şifre giriniz';
-                      }
-                      return null;
-                    },
-                    controller: passwordcontroller,
-                    decoration: InputDecoration(
-                        hintText: "Güçlü bir parola seçin",
-                        prefixIcon: Icon(Icons.lock)),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 4, 82, 111),
-                          Color.fromARGB(255, 35, 104, 130),
-                          Color.fromARGB(255, 4, 82, 111)
-                        ]),
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: Center(
-                      child: Text(
-                        "Kayıt Ol",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 25.0,
-                            fontWeight: FontWeight.bold),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Ad",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Lütfen Ad ve Soyad giriniz';
+                        }
+                        return null;
+                      },
+                      controller: namecontroller,
+                      decoration: InputDecoration(
+                          hintText: "Adınız",
+                          prefixIcon: Icon(Icons.person_outline)),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Text(
+                      "E-posta",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Lütfen E-posta giriniz';
+                        }
+                        return null;
+                      },
+                      controller: emailcontroller,
+                      decoration: InputDecoration(
+                          hintText: "E-posta adresinizi girin",
+                          prefixIcon: Icon(Icons.mail_outline)),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Text(
+                      "Şifre",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Lütfen güçlü bir şifre giriniz';
+                        }
+                        return null;
+                      },
+                      controller: passwordcontroller,
+                      decoration: InputDecoration(
+                          hintText: "Güçlü bir parola seçin",
+                          prefixIcon: Icon(Icons.lock)),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Color.fromARGB(255, 4, 82, 111),
+                            Color.fromARGB(255, 35, 104, 130),
+                            Color.fromARGB(255, 4, 82, 111)
+                          ]),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Center(
+                        child: Text(
+                          "Kayıt Ol",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Kayıt işlemi tamamlandıysa\ngiriş ekranına dönebilirsiniz",
-                        style: TextStyle(
-                            color: Color(0xFF311937),
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w300),
-                      )
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => LogIn()));
-                    },
-                    child: Row(
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Text(
-                          "Giriş Yap>>",
+                          "Kayıt işlemi tamamlandıysa\ngiriş ekranına dönebilirsiniz",
                           style: TextStyle(
                               color: Color(0xFF311937),
                               fontSize: 17.0,
-                              fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.w300),
                         )
                       ],
                     ),
-                  ),
-                ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => LogIn()));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Giriş Yap>>",
+                            style: TextStyle(
+                                color: Color(0xFF311937),
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
