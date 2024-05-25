b63b415f29bb3e2cee534a3085384b0dbe65a696
~ Khal!d <Khaledtg17@gmail.com>
2024-05-26 00:43:53 +0300

    Create booking_admin.dart

---
+++ lib/Admin/booking_admin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookingAdmin extends StatefulWidget {
+  const BookingAdmin({super.key});
+
+  @override
+  State<BookingAdmin> createState() => _BookingAdminState();
+}
+
+class _BookingAdminState extends State<BookingAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
