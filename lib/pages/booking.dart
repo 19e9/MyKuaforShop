f4f5d1e60dc419b2e1477903a531a116e0ac9ebf
~ Khal!d <Khaledtg17@gmail.com>
2024-05-25 16:17:49 +0300

    Add user booking conflict check and detailed appointment booking functionality.

--- lib/pages/booking.dart
+++ lib/pages/booking.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mykuaforshop/services/db.dart';
+import 'package:mykuaforshop/services/shared_pref.dart';
 
 class Booking extends StatefulWidget {
   String service;
@@ -11,6 +13,26 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
+  String? name, image, email;
+
+  getthedatafromsharedpref() async {
+    name = await SharedpreferenceHelper().getUserName();
+    image = await SharedpreferenceHelper().getUserImage();
+    email = await SharedpreferenceHelper().getUserEmail();
+    setState(() {});
+  }
+
+  getontheload() async {
+    await getthedatafromsharedpref();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getontheload();
+    super.initState();
+  }
+
   DateTime _selectedDate = DateTime.now();
 
   Future<void> _selectDate(BuildContext context) async {
@@ -27,12 +49,76 @@ class _BookingState extends State<Booking> {
   }
 
   TimeOfDay _selectedTime = TimeOfDay.now();
+  List<TimeOfDay> _appointments = [];
+
   Future<void> _selectTime(BuildContext context) async {
     final TimeOfDay? picked =
         await showTimePicker(context: context, initialTime: _selectedTime);
     if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
+      // Barber çalışma saatlerini kontrol etme fonksiyonu  (9.00 ile 22.00 arası)
+      if (picked.hour >= 9 && picked.hour <= 22) {
+        // Mevcut randevuları aynı anda kontrol etme
+        if (!_appointments.contains(picked)) {
+          setState(() {
+            _selectedTime = picked;
+            _appointments.add(picked);
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text(
+                    'Randevu saati başarıyla seçildi: ${picked.format(context)}')),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Bu saat için zaten bir randevu var.')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text(
+                  'Randevu saatleri sadece 9.00 ile 22.00 arasında olmalıdır.')),
+        );
+      }
+    }
+  }
+
+  Future<void> _checkAndBookAppointment(BuildContext context) async {
+    // Kullanıcının aynı hizmet için seçilen tarihte randevusu olup olmadığını kontrol et
+    bool alreadyBooked = await DatabaseMethods().checkUserBooking(
+      email!,
+      widget.service,
+      "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}",
+    );
+
+    if (alreadyBooked) {
+      // Eğer zaten randevusu varsa, kullanıcıya mesaj göster
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Bu hizmet için bu tarihte zaten bir randevunuz var.'),
+        ),
+      );
+    } else {
+      // Eğer randevusu yoksa, yeni randevu oluştur
+      Map<String, dynamic> userBookingmap = {
+        "Servis": widget.service,
+        "Randevu_Tarihi":
+            "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}"
+                .toString(),
+        "Rendavu_Saati": _selectedTime.format(context),
+        "Ad_Soyad": name,
+        "Profil_Resimi": image,
+        "E_posta": email,
+      };
+      await DatabaseMethods().addUserBooking(userBookingmap).then((value) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              "Randevunuz başarıyla oluşturuldu.",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        );
       });
     }
   }
@@ -61,7 +147,7 @@ class _BookingState extends State<Booking> {
             ),
             SizedBox(height: 30.0),
             Text(
-              "let's\njorney begin ",
+              "Hadi\nbaşlayalım ",
               style: TextStyle(
                   color: Colors.black,
                   fontSize: 20.0,
@@ -167,7 +253,7 @@ class _BookingState extends State<Booking> {
                           size: 30.0,
                         ),
                       ),
-                      SizedBox(width: 20.0),
+                      SizedBox(width: 15.0),
                       Text(
                         _selectedTime.format(context),
                         style: TextStyle(
@@ -181,20 +267,46 @@ class _BookingState extends State<Booking> {
               ),
             ),
             GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                decoration: BoxDecoration(
-                    color: Color(0xFFfe8f33),
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Center(
-                  child: Text(
-                    "Randevu Al",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
+              onTap: () async {
+                Map<String, dynamic> userBookingmap = {
+                  "Servis": widget.service,
+                  "Randevu_Tarihi":
+                      "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}"
+                          .toString(),
+                  "Randevu_Saati": _selectedTime.format(context),
+                  "Ad_Soyad": name,
+                  "Profil_Resimi": image,
+                  "E_posta": email,
+                };
+                await DatabaseMethods()
+                    .addUserBooking(userBookingmap)
+                    .then((value) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                    "Randevunuz başarıyla oluşturuldu.",
+                    style: TextStyle(fontSize: 20.0),
+                  )));
+                });
+              },
+              child: GestureDetector(
+                onTap: () {
+                  _checkAndBookAppointment(context);
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFfe8f33),
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Center(
+                    child: Text(
+                      "Randevu Al",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
