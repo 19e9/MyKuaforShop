d5ab2791d4cbbda3a2143434137a71cdcfa99423
~ Khal!d <Khaledtg17@gmail.com>
2024-05-26 13:11:21 +0300

    Bug fixes and improvements.

--- lib/Admin/booking_admin.dart
+++ lib/Admin/booking_admin.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mykuaforshop/services/db.dart';
 
 class BookingAdmin extends StatefulWidget {
   const BookingAdmin({super.key});
@@ -8,8 +11,133 @@ class BookingAdmin extends StatefulWidget {
 }
 
 class _BookingAdminState extends State<BookingAdmin> {
-  
   Stream? BookingStream;
+
+  getontheload() async {
+    BookingStream = await DatabaseMethods().getBooking();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getontheload();
+    super.initState();
+  }
+
+  Widget allBookings() {
+    return StreamBuilder(
+        stream: BookingStream,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: snapshot.data.docs.length,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.docs[index];
+                    return Material(
+                      elevation: 8.0,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Color.fromARGB(255, 4, 82, 111),
+                            Color.fromARGB(255, 65, 130, 154),
+                            Color.fromARGB(255, 4, 82, 111)
+                          ]),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(60),
+                                  child: Image.network(
+                                    ds["Profil_Resimi"],
+                                    height: 80,
+                                    width: 80,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              "Servis: " + ds["Servis"],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ), //boşluk vermek
+                            Text(
+                              "Ad_Soyad: " + ds["Ad_Soyad"],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              "Randevu_Tarihi: " + ds["Randevu_Tarihi"],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              "Randevu_Saati: " + ds["Randevu_Saati"],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            GestureDetector(
+                              onTap: () async {
+                                await DatabaseMethods().DeleteBooking(ds.id);
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 4, 82, 111),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Text(
+                                  "Tamam",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  })
+              : Container();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,99 +158,7 @@ class _BookingAdminState extends State<BookingAdmin> {
             SizedBox(
               height: 30.0,
             ),
-            Material(
-              elevation: 8.0,
-              borderRadius: BorderRadius.circular(20),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Color.fromARGB(255, 4, 82, 111),
-                    Color.fromARGB(255, 65, 130, 154),
-                    Color.fromARGB(255, 4, 82, 111)
-                  ]),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(60),
-                          child: Image.asset(
-                            "images/boy.jpg",
-                            height: 80,
-                            width: 80,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "Servis: Traş",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ), //boşluk vermek
-                    Text(
-                      "Ad_Soyad: Khalid Tariq",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Text(
-                      "Randevu_Tarihi: 26/5/2024",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Text(
-                      "Randevu_Saati: 04:20 PM",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 4, 82, 111),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Text(
-                        "Tamam",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                  ],
-                ),
-              ),
-            )
+            Expanded(child: allBookings()),
           ],
         ),
       ),
