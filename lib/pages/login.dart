f71651b75f361d620931ff177d42b361ac632881
~ Khal!d <Khaledtg17@gmail.com>
2024-05-26 17:50:06 +0300

    Bug fixes and improvements.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -47,180 +47,186 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                top: 50.0,
-                left: 30.0,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                  top: 50.0,
+                  left: 30.0,
+                ),
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 4, 82, 111),
+                  Color.fromARGB(255, 65, 130, 154),
+                  Color.fromARGB(255, 4, 82, 111)
+                ])),
+                child: Text(
+                  "Merhaba\nGiriş yap!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 4, 82, 111),
-                Color.fromARGB(255, 65, 130, 154),
-                Color.fromARGB(255, 4, 82, 111)
-              ])),
-              child: Text(
-                "Merhaba\nGiriş yap!",
-                style: TextStyle(
+              Container(
+                padding: EdgeInsets.only(
+                    top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 4),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                  top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
-              margin:
-                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 4),
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40))),
-              child: Form(
-                key: _formkey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "E-posta",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Lütfen doğru bir e-posta adres girin';
-                        }
-                        return null;
-                      },
-                      controller: emailcontroller,
-                      decoration: InputDecoration(
-                          hintText: "E-posta adresinizi girin",
-                          prefixIcon: Icon(Icons.mail_outline)),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    Text(
-                      "Şifre",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Lütfen güçlü bir şifre seçin';
-                        }
-                        return null;
-                      },
-                      controller: passwordcontroller,
-                      decoration: InputDecoration(
-                        hintText: "Şifrenizi girin",
-                        prefixIcon: Icon(Icons.lock_outline),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40))),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "E-posta",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.w500),
                       ),
-                      obscureText: true,
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    GestureDetector(
-                      onTap: (){
-                         Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => ForgotPassword()));
-                      },
-                      child: Row(
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Lütfen doğru bir e-posta adres girin';
+                          }
+                          return null;
+                        },
+                        controller: emailcontroller,
+                        decoration: InputDecoration(
+                            hintText: "E-posta adresinizi girin",
+                            prefixIcon: Icon(Icons.mail_outline)),
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Text(
+                        "Şifre",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Lütfen güçlü bir şifre seçin';
+                          }
+                          return null;
+                        },
+                        controller: passwordcontroller,
+                        decoration: InputDecoration(
+                          hintText: "Şifrenizi girin",
+                          prefixIcon: Icon(Icons.lock_outline),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotPassword()));
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Şifremi unuttum?",
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 60.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formkey.currentState!.validate()) {
+                            setState(() {
+                              mail = emailcontroller.text;
+                              password = passwordcontroller.text;
+                            });
+                            userLogin();
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10.0),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                Color.fromARGB(255, 4, 82, 111),
+                                Color.fromARGB(255, 35, 104, 130),
+                                Color.fromARGB(255, 4, 82, 111)
+                              ]),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Center(
+                              child: Text(
+                            "Giriş Yap",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Text(
-                            "Şifremi unuttum?",
+                            "Veya",
                             style: TextStyle(
                                 color: Colors.black54,
-                                fontSize: 18.0,
+                                fontSize: 17.0,
                                 fontWeight: FontWeight.w500),
                           ),
                         ],
                       ),
-                    ),
-                    SizedBox(
-                      height: 60.0,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        if (_formkey.currentState!.validate()) {
-                          setState(() {
-                            mail = emailcontroller.text;
-                            password = passwordcontroller.text;
-                          });
-                          userLogin();
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(vertical: 10.0),
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(colors: [
-                              Color.fromARGB(255, 4, 82, 111),
-                              Color.fromARGB(255, 35, 104, 130),
-                              Color.fromARGB(255, 4, 82, 111)
-                            ]),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Center(
-                            child: Text(
-                          "Giriş Yap",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold),
-                        )),
-                      ),
-                    ),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Veya",
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.w500),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SingUp()));
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Üye Ol",
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => SingUp()));
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Üye Ol",
-                            style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
